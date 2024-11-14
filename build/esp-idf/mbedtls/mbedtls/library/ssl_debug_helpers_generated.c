dcbb7edc7aaa46a115ed27f629bfbe1a2b7bed27
TIRZAH GRACE.YARRANNA <tirzahgrace@TIRZAHs-MacBook-Air.local>
2024-11-14 11:18:26 +0530

    Integrated

--- build/esp-idf/mbedtls/mbedtls/library/ssl_debug_helpers_generated.c
+++
@@ -1,1 +0,0 @@
-/Users/tirzahgrace/esp/v5.3.1/esp-idf/components/mbedtls/mbedtls/library/ssl_debug_helpers_generated.c
--- main/main_functions.cc
+++ main/main_functions.cc
@@ -28,23 +28,142 @@ limitations under the License.
 #include "tensorflow/lite/micro/micro_mutable_op_resolver.h"
 #include "tensorflow/lite/schema/schema_generated.h"
 #include "tensorflow/lite/version.h"
+#include <esp_system.h>
+#include <nvs_flash.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+
+#include "esp_camera.h"
+#include "esp_http_server.h"
+#include "esp_timer.h"
+#include "camera_pins.h"
+#include "connect_wifi.h"
 
 unsigned long start_time;
+bool wifi_setup = false;
+static const char *WEBSERVER_TAG = "esp32-cam Webserver";
+
+#define PART_BOUNDARY "123456789000000000000987654321"
+static const char *_STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
+static const char *_STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
+static const char *_STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
 
 // Globals, used for compatibility with Arduino-style sketches.
-namespace {
-tflite::ErrorReporter* error_reporter = nullptr;
-const tflite::Model* model = nullptr;
-tflite::MicroInterpreter* interpreter = nullptr;
-TfLiteTensor* input = nullptr;
+namespace
+{
+  tflite::ErrorReporter *error_reporter = nullptr;
+  const tflite::Model *model = nullptr;
+  tflite::MicroInterpreter *interpreter = nullptr;
+  TfLiteTensor *input = nullptr;
 
-// An area of memory to use for input, output, and intermediate arrays.
-constexpr int kTensorArenaSize = 93 * 1024;
-static uint8_t tensor_arena[kTensorArenaSize];
-}  // namespace
+  // An area of memory to use for input, output, and intermediate arrays.
+  // Allocate tensor_arena in PSRAM for more efficient memory management
+  constexpr int kTensorArenaSize = 93 * 1024;
+  static uint8_t *tensor_arena = (uint8_t *)heap_caps_malloc(kTensorArenaSize, MALLOC_CAP_SPIRAM | MALLOC_CAP_8BIT);
+} // namespace
+
+esp_err_t jpg_stream_httpd_handler(httpd_req_t *req)
+{
+  camera_fb_t *fb = NULL;
+  esp_err_t res = ESP_OK;
+  size_t _jpg_buf_len;
+  uint8_t *_jpg_buf;
+  char *part_buf[64];
+  static int64_t last_frame = 0;
+  if (!last_frame)
+  {
+    last_frame = esp_timer_get_time();
+  }
+
+  res = httpd_resp_set_type(req, _STREAM_CONTENT_TYPE);
+  if (res != ESP_OK)
+  {
+    return res;
+  }
+
+  while (true)
+  {
+    fb = esp_camera_fb_get();
+    if (!fb)
+    {
+      ESP_LOGE(WEBSERVER_TAG, "Camera capture failed");
+      res = ESP_FAIL;
+      break;
+    }
+    if (fb->format != PIXFORMAT_JPEG)
+    {
+      bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
+      if (!jpeg_converted)
+      {
+        ESP_LOGE(WEBSERVER_TAG, "JPEG compression failed");
+        esp_camera_fb_return(fb);
+        res = ESP_FAIL;
+      }
+    }
+    else
+    {
+      _jpg_buf_len = fb->len;
+      _jpg_buf = fb->buf;
+    }
+
+    if (res == ESP_OK)
+    {
+      res = httpd_resp_send_chunk(req, _STREAM_BOUNDARY, strlen(_STREAM_BOUNDARY));
+    }
+    if (res == ESP_OK)
+    {
+      size_t hlen = snprintf((char *)part_buf, 64, _STREAM_PART, _jpg_buf_len);
+
+      res = httpd_resp_send_chunk(req, (const char *)part_buf, hlen);
+    }
+    if (res == ESP_OK)
+    {
+      res = httpd_resp_send_chunk(req, (const char *)_jpg_buf, _jpg_buf_len);
+    }
+    if (fb->format != PIXFORMAT_JPEG)
+    {
+      free(_jpg_buf);
+    }
+    esp_camera_fb_return(fb);
+    if (res != ESP_OK)
+    {
+      break;
+    }
+    int64_t fr_end = esp_timer_get_time();
+    int64_t frame_time = fr_end - last_frame;
+    last_frame = fr_end;
+    frame_time /= 1000;
+    ESP_LOGE(WEBSERVER_TAG, "MJPG: %luKB %lums (%.1ffps)",
+             (uint32_t)(_jpg_buf_len / 1024),
+             (uint32_t)frame_time, 1000.0 / (uint32_t)frame_time);
+  }
+
+  last_frame = 0;
+  return res;
+}
+
+httpd_uri_t uri_get = {
+    .uri = "/",
+    .method = HTTP_GET,
+    .handler = jpg_stream_httpd_handler,
+    .user_ctx = NULL};
+httpd_handle_t setup_server(void)
+{
+  httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+  httpd_handle_t stream_httpd = NULL;
+
+  if (httpd_start(&stream_httpd, &config) == ESP_OK)
+  {
+    httpd_register_uri_handler(stream_httpd, &uri_get);
+  }
+
+  return stream_httpd;
+}
 
 // The name of this function is important for Arduino compatibility.
-void setup() {
+void setup()
+{
   // Set up logging. Google style is to avoid globals or statics because of
   // lifetime uncertainty, but since this has a trivial destructor it's okay.
   // NOLINTNEXTLINE(runtime-global-variables)
@@ -54,7 +173,8 @@ void setup() {
   // Map the model into a usable data structure. This doesn't involve any
   // copying or parsing, it's a very lightweight operation.
   model = tflite::GetModel(g_person_detect_model_data);
-  if (model->version() != TFLITE_SCHEMA_VERSION) {
+  if (model->version() != TFLITE_SCHEMA_VERSION)
+  {
     TF_LITE_REPORT_ERROR(error_reporter,
                          "Model provided is schema version %d not equal "
                          "to supported version %d.",
@@ -86,31 +206,47 @@ void setup() {
 
   // Allocate memory from the tensor_arena for the model's tensors.
   TfLiteStatus allocate_status = interpreter->AllocateTensors();
-  if (allocate_status != kTfLiteOk) {
+  if (allocate_status != kTfLiteOk)
+  {
     TF_LITE_REPORT_ERROR(error_reporter, "AllocateTensors() failed");
     return;
   }
 
   // Get information about the memory area to use for the model's input.
   input = interpreter->input(0);
+
+  esp_err_t err;
+
+  // Initialize NVS
+  esp_err_t ret = nvs_flash_init();
+  if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+  {
+    ESP_ERROR_CHECK(nvs_flash_erase());
+    ret = nvs_flash_init();
+  }
+
+  connect_wifi();
 }
 
 // The name of this function is important for Arduino compatibility.
-void loop() {
+void loop()
+{
   start_time = esp_log_timestamp();
 
   // Get image from provider.
   if (kTfLiteOk != GetImage(error_reporter, kNumCols, kNumRows, kNumChannels,
-                            input->data.uint8)) {
+                            input->data.uint8))
+  {
     TF_LITE_REPORT_ERROR(error_reporter, "Image capture failed.");
   }
 
   // Run the model on this input and make sure it succeeds.
-  if (kTfLiteOk != interpreter->Invoke()) {
+  if (kTfLiteOk != interpreter->Invoke())
+  {
     TF_LITE_REPORT_ERROR(error_reporter, "Invoke failed.");
   }
 
-  TfLiteTensor* output = interpreter->output(0);
+  TfLiteTensor *output = interpreter->output(0);
 
   // Process the inference results.
   uint8_t person_score = output->data.uint8[kPersonIndex];
@@ -120,4 +256,16 @@ void loop() {
   // Subtract 2000 as LED is glowing for 1000 ms in "detection responder. cc"
   TF_LITE_REPORT_ERROR(error_reporter, "time elapsed for detection: &d ms\n",
                        (esp_log_timestamp() - start_time - 1000));
+
+  if (!wifi_setup)
+  {
+    if (wifi_connect_status)
+    {
+      setup_server();
+      ESP_LOGE(WEBSERVER_TAG, "ESP32 CAM Web Server is up and running\n");
+      wifi_setup = true;
+    }
+    else
+      ESP_LOGE(WEBSERVER_TAG, "Failed to connected with Wi-Fi, check your network Credentials\n");
+  }
 }
